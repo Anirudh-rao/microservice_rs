041b729ab37928b0cdf54e0fa8d1106389762fa7
Anirudh-rao <anirudh131299@gmail.com>
2023-02-18 21:04:25 +0530

    Updates in Main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+
+extern crate hyper;
+extern crate futures;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+
+use hyper::server::{Request,Response,Service};
+use futures::future::Future;
+
+struct Microservice;
+
+impl service for Microservice{
+    type Request =  Request;
+    type Response =  Response;
+    type Error  =  hype::Error;
+    type Future =  Box<Future<Item = Self::Response, Error =  self::Error>>;
+
+}
+
+
+
+fn call(&self, request::Request) -> Self::Future{
+    info!("Microservice Recieved a request:{;?}", request);
+    Box::new(futures::future::ok(Reponse::New()))
 }
