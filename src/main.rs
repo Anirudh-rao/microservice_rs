276458ffdc55faa8b002deb911328d3e8d43c855
Anirudh-rao <anirudh131299@gmail.com>
2023-02-19 11:01:51 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,19 @@ impl service for Microservice{
     type Error  =  hype::Error;
     type Future =  Box<Future<Item = Self::Response, Error =  self::Error>>;
 
+    fn call(&self, request::Request) -> Self::Future{
+        info!("Microservice Recieved a request:{;?}", request);
+        Box::new(futures::future::ok(Reponse::New()))
+    }
+
 }
 
-
-
-fn call(&self, request::Request) -> Self::Future{
-    info!("Microservice Recieved a request:{;?}", request);
-    Box::new(futures::future::ok(Reponse::New()))
+fn main() {
+    env_logger::init();
+    let address = "127.0.0.1:8080".parse().unwrap();
+    let server = hyper::server::Http::new()
+        .bind(&address, || Ok(Microservice {}))
+        .unwrap();
+    info!("Running microservice at {}", address);
+    server.run().unwrap();
 }
